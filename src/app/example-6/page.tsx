7b280ec34993709636048d70f4f5053b64ded205
DominicDev <s0l0estudi4r@gmail.com>
2025-12-19 14:21:28 +0100

    v0.0.4

---
+++ src/app/example-6/page.tsx
@@ -0,0 +1,92 @@
+'use client';
+import './page.css';
+import {
+  addTransitionType,
+  startTransition,
+  useState,
+  ViewTransition
+} from 'react';
+
+function Component() {
+  const [show, setShow] = useState(false);
+
+  function handlePrev() {
+    startTransition(() => {
+      addTransitionType(show ? 'slide-out-left' : 'slide-in-right');
+      setShow(prev => !prev);
+    });
+  }
+
+  function handleNext() {
+    startTransition(() => {
+      addTransitionType(show ? 'slide-out-right' : 'slide-in-left');
+      setShow(prev => !prev);
+    });
+  }
+
+  return (
+    <>
+      <section className='container-buttons'>
+        {[
+          {
+            fn: handlePrev,
+            label: '⬅️'
+          },
+          {
+            fn: handleNext,
+            label: '➡️'
+          }
+        ].map(({ fn, label }, i) => (
+          <button key={i} className='button-show-or-not' onClick={fn}>
+            {label}
+          </button>
+        ))}
+      </section>
+
+      {show ? (
+        <ViewTransition
+          key='visible'
+          enter={{
+            'slide-in-left': 'slide-in-left',
+            'slide-in-right': 'slide-in-right'
+          }}
+          exit={{
+            'slide-out-left': 'slide-out-left',
+            'slide-out-right': 'slide-out-right'
+          }}
+        >
+          <aside className='rectangle rectangle-gradient' />
+        </ViewTransition>
+      ) : (
+        <ViewTransition
+          key='hidden'
+          enter={{
+            'slide-in-left': 'slide-in-left',
+            'slide-in-right': 'slide-in-right'
+          }}
+          exit={{
+            'slide-out-left': 'slide-out-left',
+            'slide-out-right': 'slide-out-right'
+          }}
+        >
+          <aside className='rectangle rectangle-hidden' />
+        </ViewTransition>
+      )}
+    </>
+  );
+}
+
+export default function Page() {
+  return (
+    <article className='page'>
+      <video
+        className='am-video'
+        src='/assets/v1deo-example-6.mp4'
+        autoPlay
+        muted
+        loop
+      />
+      <Component />
+    </article>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from 'next/navigation';
 
 export default function Home() {
-  redirect('/example-5');
+  redirect('/example-6');
   return <></>;
 }
