7253761d0ba557f669cc09590680bb2da342d2a8
DominicDev <s0l0estudi4r@gmail.com>
2025-12-21 17:35:24 +0100

    v0.2.3

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -2,7 +2,14 @@ import '@/styles/hero.css';
 export function Hero() {
   return (
     <article className='hero'>
-      <h1 className='title-1'>20 Examples with View Transitions</h1>
+      <h1 className='title-1'>
+        20 Examples with View Transitions{' '}
+        <img
+          src='/assets/view-transtition-logo.svg'
+          alt='view-transtion'
+          className='am-view-transition-logo'
+        />
+      </h1>
     </article>
   );
 }
